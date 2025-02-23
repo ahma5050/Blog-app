9f5c563d2dcdea161535dc686ff23c3f3776df4e
Ahmisha5050 <tekisha5050@gmail.com>
2025-02-23 13:30:12 +0300

    responsive navbar using flowbit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,9 +6,11 @@ import Projects from './pages/Projects'
 import Signin from './pages/Signin'
 import Signup from './pages/Signup'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Header from './components/Header'
 const App = () => {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/About" element={<About />} />
